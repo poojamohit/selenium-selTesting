e0a780a77d4183da33345d693fec7191c7e545f9
poojamohit <33201423+poojamohit@users.noreply.github.com>
2018-09-01 18:26:05 +0530

    Update addressbook.java

--- webapp-addresbbokproject/src/test/java/TestNG/addressbook.java
+++ webapp-addresbbokproject/src/test/java/TestNG/addressbook.java
@@ -23,11 +23,11 @@ public class TestngParameters {
  
       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
       driver.get("http://ec2-54-172-142-110.compute-1.amazonaws.com:3001/");
-      driver.findElement(By.xpath(".//*[@id='account']/a")).click();
-      driver.findElement(By.id("log")).sendKeys(sUsername);
-      driver.findElement(By.id("pwd")).sendKeys(sPassword);
-      driver.findElement(By.id("login")).click();
-      driver.findElement(By.xpath(".//*[@id='account_logout']/a")).click();
+      driver.findElement(By.xpath("./html/body/form/div/div/div/div[1]/div[2]/input")).click();
+      driver.findElement(By.id("login")).sendKeys(sUsername);
+      driver.findElement(By.id("password")).sendKeys(sPassword);
+      driver.findElement(By.id("click")).click();
+      driver.findElement(By.xpath("./html/body/form/div/div/div/div[1]/div[2]/input")).click();
       driver.quit();
 
   }
